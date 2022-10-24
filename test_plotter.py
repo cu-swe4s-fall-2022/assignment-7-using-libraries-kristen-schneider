d50433e7c4a7b571acf0fbc047e8d954193fa7cc
kristen-schneider <kristen.schneider@colorado.edu>
2022-10-24 23:15:19 +0000

    adding unit tests for plotting

--- test_plotter.py
+++ test_plotter.py
@@ -1,8 +1,28 @@
-import data_processor as dp
+import plotter
 import os
 import random
 import unittest
 
 class TestPlotter(unittest.TestCase):
     
-    def test_get_random_matrix(self):
+    def test_boxplot(self):
+        bad_data_file = 'bad_iris.data'
+        out_png = 'iris_boxplot.png'
+        # raises exception
+        self.assertRaises(Exception, plotter.make_boxplot, bad_data_file, out_png)
+    
+    def test_scatterplot(self):
+        bad_data_file = 'bad_iris.data'
+        out_png = 'iris_boxplot.png'
+        # raises exception
+        self.assertRaises(Exception, plotter.make_boxplot, bad_data_file, out_png)
+    
+    def test_combine_plots(self):
+        bad_data_file = 'bad_iris.data'
+        out_png = 'iris_boxplot.png'
+        # raises exception
+        self.assertRaises(Exception, plotter.make_boxplot, bad_data_file, out_png)
+        
+if __name__ == '__main__':
+    unittest.main()
+    

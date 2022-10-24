e4554b4cfdba9909fb6fa341204f09757207e86e
kristen-schneider <kristen.schneider@colorado.edu>
2022-10-24 23:26:33 +0000

    adding print statements

--- run.sh
+++ run.sh
@@ -1,13 +1,17 @@
 #! bin/bash
 
 # unit tests for data processor
+echo "Unit tests for data processor"
 python test_data_processor.py
 
 # functional tests for data processor
+echo "Functional tests for data processor"
 bash test_process.sh
 
 # unit tests for plotter
+echo "Unit tests for plotting"
 python test_plotter.py
 
 # functional tests for plotter
+echo "Functional tests for plotting"
 bash test_plot.sh
